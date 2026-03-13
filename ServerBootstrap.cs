ca1bba24445dc92e9fe9bc70d63e48d28cc20ffa
Lachezar Tsochev <ltsochev@live.com>
2026-03-13 18:03:04 +0200

    initial repo setup

---
+++ ServerBootstrap.cs
@@ -0,0 +1,26 @@
+﻿namespace ServerBootstrap;
+
+using CounterStrikeSharp.API;
+using CounterStrikeSharp.API.Core;
+using CounterStrikeSharp.API.Core.Attributes;
+using CounterStrikeSharp.API.Core.Attributes.Registration;
+using Microsoft.Extensions.Logging;
+
+public class ServerBootstrap : BasePlugin
+{
+    public override string ModuleName => "ServerBootstrap";
+    public override string ModuleVersion => "0.0.1";
+    public override string ModuleAuthor => "ltsochev-dev";
+    public override string ModuleDescription => "Bootstraps the servers properly according to Agones annotations.";
+    private AgonesSDK agones;
+
+    public ServerBootstrap()
+    {
+        agones = new AgonesSDK(logger: Logger);
+    }
+
+    public override void Load(bool hotReload)
+    {
+        // @todo Use WatchServer
+    }
+}
