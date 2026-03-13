4eb0904f20c64cb45e7de9e16c48eca9d242c5d3
Lachezar Tsochev <ltsochev@live.com>
2026-03-13 22:07:26 +0200

    testing plugin

--- ServerBootstrap.cs
+++ ServerBootstrap.cs
@@ -1,11 +1,15 @@
 ﻿namespace ServerBootstrap;
 
+using Agones;
+using Agones.Dev.Sdk;
 using CounterStrikeSharp.API;
 using CounterStrikeSharp.API.Core;
 using CounterStrikeSharp.API.Core.Attributes;
 using CounterStrikeSharp.API.Core.Attributes.Registration;
 using Microsoft.Extensions.Logging;
+using System.Collections.Concurrent;
 
+[MinimumApiVersion(80)]
 public class ServerBootstrap : BasePlugin
 {
     public override string ModuleName => "ServerBootstrap";
@@ -13,6 +17,7 @@ public class ServerBootstrap : BasePlugin
     public override string ModuleAuthor => "ltsochev-dev";
     public override string ModuleDescription => "Bootstraps the servers properly according to Agones annotations.";
     private AgonesSDK agones;
+    private readonly ConcurrentQueue<Action> _mainThreadActions = new();
 
     public ServerBootstrap()
     {
@@ -21,6 +26,39 @@ public class ServerBootstrap : BasePlugin
 
     public override void Load(bool hotReload)
     {
-        // @todo Use WatchServer
+        Logger.LogInformation("[Bootstrap] Load: Plugin loading...");
+
+        agones.WatchGameServer(OnGameServerChange);
+    }
+
+    private void OnGameServerChange(GameServer gs)
+    {
+        if (gs == null)
+        {
+            Logger.LogWarning("[Bootstrap] GS Update: GameServer update was null");
+            return;
+        }
+
+        var state = gs.Status?.State ?? "Unknown";
+        var name = gs.ObjectMeta?.Name ?? "Unknown";
+
+        Logger.LogInformation("[Bootstrap] GS Update: {Name}, state={State}", name, state);
+
+        if (gs.ObjectMeta?.Annotations != null)
+        {
+            foreach (var kv in gs.ObjectMeta.Annotations)
+            {
+                Logger.LogInformation("Annotation {Key}={Value}", kv.Key, kv.Value);
+            }
+        }
+
+        Logger.LogInformation(state);
+    }
+
+    public override void Unload(bool hotReload)
+    {
+        Logger.LogInformation("[Bootstrap] Unload: Plugin unloading...");
+
+        base.Unload(hotReload);
     }
 }
